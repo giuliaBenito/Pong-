837641eea3819c5ac92ecb83abb58a0cf0ddb4ab
GiuliaFernandes <giulia.abreu2004@hotmail.com>
2024-11-08 17:04:20 -0300

    movimentação básica + limite de tela

--- Scenes/Jogador/jogador.gd
+++ Scenes/Jogador/jogador.gd
@@ -11,11 +11,25 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	pass
+	movimentar_jogador(delta)
+	limitar_movimento_do_jogador()
 
-
-func movimentar_jogador() -> void:
+func movimentar_jogador(delta : float) -> void:
+#movimento do jogador 1
 	if jogador1: 
 		if Input.is_action_pressed("mv-cima-1"):
-		pass
+			position.y -= velocidade_do_jogador * delta
+		elif Input.is_action_pressed("mv-baixo-1"):
+			position.y += velocidade_do_jogador * delta
 	
+	#movimento do jogador 2
+	else:
+		if Input.is_action_pressed("mv-cima-2"):
+			position.y -= velocidade_do_jogador * delta
+		elif Input.is_action_pressed("mv-baixo-2"):
+			position.y += velocidade_do_jogador * delta
+
+
+func limitar_movimento_do_jogador() -> void:
+	#limitação do jogador na tela
+	position.y = clamp(position.y, 64, 654)
